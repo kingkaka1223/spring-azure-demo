e10e4d7fdf264c13773eb9170fd22f682500f654
rxavio <rucahoxavi@gmail.com>
2026-03-16 11:59:20 +0200

    first commit

---
+++ src/main/java/com/javainuse/javainuse_azure/JavainuseAzureApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse.javainuse_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavainuseAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavainuseAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javainuse/javainuse_azure/JavainuseAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainuse.javainuse_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavainuseAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
